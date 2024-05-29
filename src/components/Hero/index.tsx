d4831e52c32255138c8a527f96a09c82896b93c8
Bruno <bdorigon@hotmail.com>
2024-05-29 18:07:45 -0300

    tirando css e mudando para typescript em todas as areas

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,13 @@
-import styles from './Hero.module.css'
+import { ContainerHero, FormHero, HeroTitle } from './style'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <FormHero>
+    <ContainerHero>
+      <HeroTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </HeroTitle>
+    </ContainerHero>
+  </FormHero>
 )
 
 export default Hero
